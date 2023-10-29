2a7c04c9addbcbd085621a19657cdfe5789a7a55
CatalinOSBI <garjoaba_c@yahoo.com>
2023-10-29 11:36:41 +0200

    loading screen complete

--- src/pages/LoadingPage/Model.js
+++ src/pages/LoadingPage/Model.js
@@ -118,10 +118,10 @@ let bgColor = '#e60000'
         camera={{ fov: 75, position: [0, 0, 2] }}>
         {/* <color attach='background' args={[bgColor]}/> */}
       <ambientLight intensity={1.1} />
-      <spotLight position={[1, 6, 1.5]} angle={2} penumbra={1} intensity={100} castShadow shadow-mapSize={[2048, 2048]} color='#4e82b4' />
-      <spotLight position={[5, 5, -5]} angle={0.3} penumbra={1} intensity={150} castShadow={true} shadow-mapSize={[256, 256]} color="#4e82b4" />
-      <spotLight position={[-2, 4.8, 0.3]} angle={3} penumbra={1} intensity={80} castShadow={true} shadow-mapSize={[256, 256]} color="#FAF7B8" />
-      <spotLight position={[-1.5, 13.6, -0.3]} angle={0.6} penumbra={1} intensity={1000} castShadow={true} shadow-mapSize={[256, 256]} color="violet" />
+      <spotLight position={[1, 6, 1.5]} angle={2} penumbra={1} intensity={500} castShadow shadow-mapSize={[2048, 2048]} color='darkred' />
+      <spotLight position={[5, 5, -5]} angle={0.3} penumbra={1} intensity={150} castShadow={true} shadow-mapSize={[256, 256]} color="red" />
+      <spotLight position={[-2, 4.8, 0.3]} angle={3} penumbra={1} intensity={80} castShadow={true} shadow-mapSize={[256, 256]} color="red" />
+      <spotLight position={[-1.5, 13.6, -0.3]} angle={0.6} penumbra={1} intensity={1000} castShadow={true} shadow-mapSize={[256, 256]} color="red" />
       <hemisphereLight position={[0.272,10,0]} intensity={0.7}/>
       <Sneaker position={[0,0,0.3]} scale={[0.7,0.7,0.7]}/>
       </Canvas>
