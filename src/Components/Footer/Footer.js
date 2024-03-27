6fd645f5927c17ddd01359622b3d106088a10826
CatalinOSBI <garjoaba_c@yahoo.com>
2024-03-27 16:40:26 +0200

    responsive product page

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import './Footer.css'
 import { useLocation } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 
 const Footer = () => {
 
   let location = useLocation()
-  let page =location.pathname.split('/')[1]
+  let page = location.pathname.split('/')[1]
+
+  const isPhone = useMediaQuery({ query: '(max-width: 600px)' })
+
+  const position = page ==='Product' && isPhone ? 'absolute' : (page ==='Product' ? 'absolute' : 'absolut')
 
   return (
-      <footer style={{position:`${page === 'Product' ? 'relative' : 'absolute'}`}}>
-        <p>MaxiM&trade;</p>
-      </footer>
+    <footer style={{ position: `${page ==='Product' && isPhone ? 'absolute' : (page ==='Product' ? 'relative' : 'absolute')}` }}>
+      <p>MaxiM&trade;</p>
+      <button onClick={()=>{console.log(position)}}>asd</button>
+    </footer>
   )
 }
 
